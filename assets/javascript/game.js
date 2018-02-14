f48d798cc15d06d562dc568121effaf5ee6abb4a
Paul Cheno <pwchenoweth@gmail.com>
2018-02-13 21:40:01 -0600

    oh got the object and function working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,67 @@
     // Creates an array that lists out all of the options a-z.
-    var computerChoices = ["THE FACES", "AL STEWART", "TODD RUNDGREN", "GERRY RAFFERTY","STEALERS WHEEL"];
+    var computerChoices = ["THE FACES", "AL STEWART", "TODD RUNDGREN", "GERRY RAFFERTY", "STEALERS WHEEL"];
 
     // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-    var wins = 0;
-    var losses = 0;
-    var guess = "";
-    var guessStr = "";
-    var guessLeft = 12;
-    var trii = -1; //negative identifies first time through
-    var winFlag = false;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    var disStr = computerGuess;
-    var band = "????????";
+
+    var game = {
+      "wins": 0,
+      "losses": 0,
+      "guess": "",
+      "guessStr": "",
+      "guessLeft": 12,
+      "band": "????????",
+      "disStr": computerGuess
+    };
+
+    var winFlag = false;
+    var trii = -1; //negative identifies first time through
+
+    //function to recreate the display word
+    function createDisStr() {
+      game.disStr = computerGuess;
+      for (var i = 0; i < computerGuess.length; i++) {
+        if (game.guessStr.includes(computerGuess[i]) == false) {
+          //letter of computerguess is NOTin the list of guessed letters. hide it "-"
+          game.disStr = game.disStr.replace(computerGuess[i], "-");
+        }
+      }
+    }
 
     // This function is run whenever the user presses a key.
     document.onkeyup = function (event) {
 
-      // Determines which key was pressed.
-      var guess = event.key;
-          guess = guess.toUpperCase();
-
+      game.guess = event.key.toUpperCase();
 
       //check to see if we are just starting or have been playing
       if (trii !== -1) {
         // If we choose the same thing as the computer, modify varibles
-        var guessed = guessStr.includes(guess);
+
         //if we have not guessed the letter before...keep on going.
-        if (guessed == false) {
+        if (!game.guessStr.includes(game.guess)) {
           // add guess to guessStr
-          if (guessStr == "") {
-            guessStr = " " + guess;
+          if (game.guessStr == "") {
+            game.guessStr = " " + game.guess;
           } else {
-            guessStr = guessStr + "," + guess;
+            game.guessStr = game.guessStr + "," + game.guess;
           }
 
           //is letter contained in the word?
-          var found = computerGuess.includes(guess);
-          if (found == true) {
+
+          if (computerGuess.includes(game.guess)) {
             // now add letter to guessStr, loop through computerguess by letter checking for the letter in guessStr
             //while looping through recreate disStr. When disStr equals computerguess thats a win
 
-            disStr = computerGuess;
-            for (var i = 0; i < computerGuess.length; i++) {
-              if (guessStr.includes(computerGuess[i]) == false) {
-                //letter of computerguess is NOTin the list of guessed letters. hide it "-"
-                disStr = disStr.replace(computerGuess[i], "-");
-
-
-              } 
-            }
+            createDisStr();
 
           } else { //(found == true) guess is in the word the else is not in the word
 
             trii++;
-            guessLeft--;
+            game.guessLeft--;
 
           } // guess is not in word
-          if (disStr == computerGuess) {
-            wins++;
+          if (game.disStr == computerGuess) {
+            game.wins++;
             winFlag = true;
             //and start the game over.
             trii = 12;
@@ -70,19 +74,14 @@
       //Was it the 12th try.If so, reset varibles, and check for a loss
 
       if (trii > 11) {
-        guessLeft = 12;
-        guessStr = "";
+        game.guessLeft = 12;
+        game.guessStr = "";
         trii = 0;
-        band =computerGuess;
+        game.band = computerGuess;
         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        disStr = computerGuess;
-        for (var i = 0; i < computerGuess.length; i++) {
-          if (computerGuess[i] !== " ") {
-            disStr = disStr.replace(computerGuess[i], "-");
-          }
-        }
+        createDisStr();
         if (winFlag == false) {
-          losses++
+          game.losses++;
         } else {
           winFlag = false;
         }
@@ -92,7 +91,7 @@
           trii = 0;
           for (var i = 0; i < computerGuess.length; i++) {
             if (computerGuess[i] !== " ") {
-              disStr = disStr.replace(computerGuess[i], "-");
+              game.disStr = game.disStr.replace(computerGuess[i], "-");
             }
           }
         }
@@ -102,12 +101,12 @@
       // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses,
       // and wins/losses/guesses left/string of guesses.
       var html =
-        "<p>Wins: " + wins + "</p>" +
-        "<p>Losses: " + losses + "</p>" +
-        "<p>Guesses Left: " + guessLeft + "</p>" +
-        "<p>Guessed Letters: " + guessStr + "</p>" +
-        "<p>Current word: " + disStr + "</p>" +
-        "<p>Band: " + band + "</p>";
+        "<p>Wins: " + game.wins + "</p>" +
+        "<p>Losses: " + game.losses + "</p>" +
+        "<p>Guesses Left: " + game.guessLeft + "</p>" +
+        "<p>Guessed Letters: " + game.guessStr + "</p>" +
+        "<p>Current word: " + game.disStr + "</p>" +
+        "<p>Band: " + game.band + "</p>";
 
       // Set the inner HTML contents of the #game div to our html string
       document.querySelector("#game").innerHTML = html;
